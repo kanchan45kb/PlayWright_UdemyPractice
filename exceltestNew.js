1f1ac399e98d3e21271da529705219ece5f9a995
kanchan45kb <68049878+kanchan45kb@users.noreply.github.com>
2026-07-09 15:46:18 +0530

    Add files via upload

--- exceltestNew.js
+++ exceltestNew.js
@@ -1,5 +1,5 @@
 const excelsheetNew = require('exceljs');
-
+/*
 async function writeExcel(searchvalue,replacevalue,filepath) {
   
     const testexcel = new excelsheetNew.Workbook();
@@ -26,4 +26,35 @@ async function readExcel(worksheet,searchvalue){
         })
         return output;
 }
-writeExcel("Lilly","Joe","E:\\exceldemotest.xlsx")
+writeExcel("Lilly","Joe","E:\\exceldemotest.xlsx")*/
+
+/*********************************************************************************************/
+
+
+async function writeExcel(searchvalue,replacevalue,change,filepath) {
+  
+    const testexcel = new excelsheetNew.Workbook();
+    await testexcel.xlsx.readFile(filepath);
+    const worksheet = testexcel.getWorksheet('Sheet2');   
+  const output = await readExcel(worksheet,searchvalue,change);
+     const cell= worksheet.getCell(output.row,output.column+change.columnchange);
+    cell.value = replacevalue;
+    await testexcel.xlsx.writeFile(filepath);
+    console.log(cell.value)
+  
+}   
+
+async function readExcel(worksheet,searchvalue){
+      let output = {row:-1,column:-1};
+        worksheet.eachRow((row,rowNumber)=> {
+                row.eachCell((cell,colNumber)=>{
+                   if(cell.value === searchvalue){
+                    output.row = rowNumber;
+                    output.column = colNumber;
+                   }
+                            })
+    
+        })
+        return output;
+}
+writeExcel("Rocky",'VIII',{rowchange:0,columnchange:1},"E:\\exceldemotest.xlsx")
